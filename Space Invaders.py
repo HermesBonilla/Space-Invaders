aacc92db8e1c31a11f6132369c47e461004ed9f9
Hermes Bonilla <hboni008@fiu.edu>
2020-08-10 19:19:40 -0400

    add font, abstract ship class, and player movement

--- Space Invaders.py
+++ Space Invaders.py
@@ -4,6 +4,7 @@ import pygame
 import os
 import time 
 import random
+pygame.font.init()
 
 # Game window
 WIDTH, HEIGHT = 750, 750
@@ -30,13 +31,46 @@ YELLOW_LASER = pygame.image.load(os.path.join(assets_path, "pixel_laser_yellow.p
 # Background
 BG = pygame.transform.scale(pygame.image.load(os.path.join(assets_path, "background-black.png")), (WIDTH, HEIGHT))
 
+# Abstract ship class
+class Ship:
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_img = None
+        self.ship_img = None
+        self.laser_img = None
+        self.lasers = []
+        self.cool_down_counter = 0
+        
+    def draw(self, window):
+        pygame.draw.rect(window, (255,0,0), (self.x, self.y, 50, 50))
+
+
+
+
 def main():
     run = True
     FPS = 60
+    level = 1
+    lives = 5
+    main_font = pygame.font.SysFont("comicsans", 50)
+    player_vel = 5 # velocity
+
+    ship = Ship(300, 650)
+
     clock = pygame.time.Clock()
 
     def redraw_window():
         WIN.blit(BG, (0,0))
+        # draw text
+        lives_label = main_font.render(f"Lives: {lives}", 1, (255,255,255))
+        level_label = main_font.render(f"Level: {level}", 1, (255,255,255))
+        WIN.blit(lives_label, (10,10))
+        WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+
+        ship.draw(WIN)
+
         pygame.display.update()
 
     while run:
@@ -46,6 +80,17 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a] and ship.x - player_vel > 0: # left
+            ship.x -= player_vel
+        if keys[pygame.K_d] and ship.x + player_vel + 50 < WIDTH: # right
+            ship.x += player_vel
+        if keys[pygame.K_w] and ship.y - player_vel > 0: # up
+            ship.y -= player_vel
+        if keys[pygame.K_s] and ship.y + player_vel + 50 < HEIGHT: # down
+            ship.y += player_vel
+
             
 
 
