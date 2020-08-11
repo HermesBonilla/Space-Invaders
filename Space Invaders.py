5eda4f425b07b92c9804616fc497b0cc8a5001ed
Hermes Bonilla <hboni008@fiu.edu>
2020-08-11 13:53:39 -0400

    add collision logic using masks

--- Space Invaders.py
+++ Space Invaders.py
@@ -31,6 +31,26 @@ YELLOW_LASER = pygame.image.load(os.path.join(assets_path, "pixel_laser_yellow.p
 # Background
 BG = pygame.transform.scale(pygame.image.load(os.path.join(assets_path, "background-black.png")), (WIDTH, HEIGHT))
 
+class Laser:
+    def __init__(self, x, y, img):
+        self.x = x
+        self.y = y
+        self.img = img
+        self.mask = pygame.mask.from_surface(self.img)
+
+    def draw(self, window):
+        window.blit(self.img, (self.x, self.y))
+
+    def move(self, vel):
+        self.y += vel
+
+    def off_screen(self, height):
+        return self.y <= height and self.y >= 0
+
+    def collision(self, obj):
+        return collide(self, obj)
+
+
 # Abstract ship class
 class Ship:
     def __init__(self, x, y, health=100):
@@ -75,6 +95,11 @@ class Enemy(Ship):
     
     def move(self, vel):
         self.y += vel
+
+def collide(obj1, obj2):
+    offset_x = obj2.x - obj1.x
+    offset_y = obj2.y - obj1.y
+    return obj1.mask.overlap(obj2.mask, (offset_x, offset_y)) != None
 
 def main():
     run = True
